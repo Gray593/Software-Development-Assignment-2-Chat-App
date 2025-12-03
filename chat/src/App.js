160a5d81163bcb684b9fa1195437c9243fd60773
Gray593 <btgray6@gmail.com>
2025-12-03 15:15:00 +0000

    displays messages

--- chat/src/App.js
+++ chat/src/App.js
@@ -1,16 +1,16 @@
-import logo from './logo.svg';
+import Message from './components/Message';
 import './App.css';
 
 function App() {
 
-  let messages = ["hello","hi","bye"]
+  let messages = [{username : "A",message : "hello",},{username : "A",message : "hello",},{username : "A",message : "hello",}]
   return (
     <div className="App">
           <div className='header'>
             <h1 className='recName'>PlaceHolder</h1>
           </div>
           <div className='messages'>
-            {messages.map(e => <Message user={username} text = {e}/>)}
+            {messages.map(e => <Message username={e.username} message = {e.message} time = {Date()}/>)}
           </div>
           <div className='newMessage'>
             <input className='userInput'>
---
+++ chat/src/components/Message.js
@@ -0,0 +1,14 @@
+
+function Message ({username, message, time }){
+
+    return(
+        <div className="messageBody">
+            <p className="messageContent">{message}</p>
+            <p className="sender">{username}</p>
+            <p className="timeStamp">{time}</p>
+        </div>
+    )
+    
+}
+
+export default Message
