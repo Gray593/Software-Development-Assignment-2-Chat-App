189e116e81e994021293ed2823d6382b29b06e01
Gray593 <btgray6@gmail.com>
2025-12-04 10:01:31 +0000

    messages now update locally

--- chat/src/App.js
+++ chat/src/App.js
@@ -1,22 +1,35 @@
 import Message from './components/Message';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [messages, setMessages] = useState([])
+ 
+function handleClick() {
+  const input = document.getElementsByClassName("userInput")[0].value;
 
-  let messages = [{username : "A",message : "hello",},{username : "A",message : "hello",},{username : "A",message : "hello",}]
+  const newMessage = {
+    username: "username",
+    message: input,
+    time: Date(),
+  };
+
+  setMessages([...messages, newMessage]); 
+
+}
   return (
     <div className="App">
           <div className='header'>
             <h1 className='recName'>PlaceHolder</h1>
           </div>
           <div className='messages'>
-            {messages.map(e => <Message username={e.username} message = {e.message} time = {Date()}/>)}
+            {messages.map(e => <Message username={e.username} message = {e.message} time = {e.time}/>)}
           </div>
           <div className='newMessage'>
             <input className='userInput'>
               
             </input>
-            <button className='sendButton'>
+            <button className='sendButton' onClick={handleClick}>
               S
             </button>
           </div>
