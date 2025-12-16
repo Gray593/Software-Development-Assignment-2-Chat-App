5e978ba0cefd4110830f9b42986382fd23e24e7d
Gray593 <btgray6@gmail.com>
2025-12-16 20:51:06 +0000

    commenting finished for the server

--- chat/backEnd/backEnd.js
+++ chat/backEnd/backEnd.js
@@ -2,29 +2,29 @@ import express from "express"
 import http from "http"
 import {Server} from "socket.io"
 
-const app = express();
-const server = http.createServer(app)
+const app = express() //creates an express instance
+const server = http.createServer(app) //wraps the express instance in a http server to allow socket to connect
 
-const io = new Server(server, {
-    cors: { origin: "*" }  
+const io = new Server(server, { //creates the server
+    cors: { origin: "*" }  //allows the react frontend to connect
 })
 
-let users = 0
+let users = 0 //this variable tracks the number of connected users
 
-io.on("connection", (socket) => {
-    users++
-    console.log("A user connected (" + users + " online)")
+io.on("connection", (socket) => {//when a user connects
+    users++ //add 1 to the user count
+    console.log("A user connected") //ouputs when a user connects used in testing
 
-    socket.on("chatMessage", (msg) => {
-        io.emit("chatMessage", msg)
-    });
+    socket.on("chatMessage", (msg) => { //listens for messages 
+        io.emit("chatMessage", msg) //broadcasts any message that is sent
+    })
 
-    socket.on("disconnect", () => {
-        users--
-        console.log("User disconnected (" + users + " online)")
-    });
+    socket.on("disconnect", () => { // when a user disconnects
+        users-- // subtratcs one from users
+        console.log("A user disconnected") // console logs a user as disconnected useful when testing
+    })
 });
 
-server.listen(3001, () => {
-    console.log("Server running on port 3001")
+server.listen(3001, () => { // starts the server on port 3001
+    console.log("Server running on port 3001") //console logs that the server is online
 });
