ae0ffa747a0526865f14ea2e3ea321c084dd3904
Gray593 <btgray6@gmail.com>
2025-12-16 20:38:39 +0000

    commenting finished on the message component

--- chat/frontEnd/src/App.js
+++ chat/frontEnd/src/App.js
@@ -11,45 +11,45 @@ export default function App() { //main function of the program that is rendered 
 
   function joinChat() {// the join chat function ensures the users input name isnt blank or just spaces
     if (username.trim() !== "") {
-      setJoined(true)//if the username is valid 
+      setJoined(true)//if the username is valid set joined to true
     }
   }
 
-  function handleClick() {
-    const inputBox = document.getElementsByClassName("userInput")[0]
-    const input = inputBox.value
+  function handleClick() { // the handle click function sends the users message to the server
+    const inputBox = document.getElementsByClassName("userInput")[0] //assigns the input box variable to the input box on the webpage
+    const input = inputBox.value //sets the input to the value within the inputbox
 
-    if (input.trim() === "") return
+    if (input.trim() === "") return // if the input is blank or just spaces exit out the function
 
-    const newMessage = {
-      username: username,
+    const newMessage = { //creates the newMessage object
+      username: username, //sets the username,message and time as individual pairs
       message: input,
       time: new Date().toLocaleTimeString()
     }
 
-    socket.emit("chatMessage", newMessage)
-    inputBox.value = ""
+    socket.emit("chatMessage", newMessage) //sends the message to the server
+    inputBox.value = "" //resets the message input to nothing
   }
 
-  useEffect(() => {
-    socket.on("chatMessage", (msg) => {
-      setMessages(prev => [...prev, msg])
+  useEffect(() => { // this use effects live updates for users chat messages 
+    socket.on("chatMessage", (msg) => { // recieves chat message
+      setMessages(prev => [...prev, msg])//adds the message to the messages array rerendering the page
     })
 
-    return () => socket.off()
+    return () => socket.off()// clean up function that removes event listeners to prevent duplicate listeners
   }, [])
 
-  if (!joined) {
+  if (!joined) { //if the user has not joined (the joined variable is false) render the following
     return (
       <div className="App joinScreen">
         <h2>Enter Name</h2>
         <input 
           placeholder="Username"
           className="userInput"
-          onChange={(e)=> setUsername(e.target.value)}
+          onChange={(e)=> setUsername(e.target.value)} // when anything is input update the username variable
         />
-        <button onClick={joinChat} className="sendButton">Join</button>
-      </div>
+        <button onClick={joinChat} className="sendButton">Join</button> 
+      </div> //above is the join button that runst the joinChat function when clicked 
     )
   }
 
@@ -67,13 +67,13 @@ export default function App() { //main function of the program that is rendered 
             message={e.message} 
             time={e.time} 
             currentUser = {username}
-          />
-        )}
+          /> // renders each message in the message array as a seperate message component 
+        )} 
       </div>
 
       <div className='newMessage'>
         <input className='userInput'/>
-        <button className='sendButton' onClick={handleClick}>
+        <button className='sendButton' onClick={handleClick}> 
           S
         </button>
       </div>
--- chat/frontEnd/src/components/Message.js
+++ chat/frontEnd/src/components/Message.js
@@ -1,7 +1,7 @@
-export default function Message ({username, message, time, currentUser }){
-    const isMe = username === currentUser
-    return(
-        <div className={`messageBody ${isMe ? "me" : "other"}`}>
+export default function Message ({username, message, time, currentUser }){ //the message, component takes in the username message, time and current user variables as props
+    const isMe = username === currentUser // determines if the message sender is the user or another user
+    return( //the below line determines the classname based on who the sender of the message is this is used for css styling
+        <div className={`messageBody ${isMe ? "me" : "other" }`}> 
             <p className="messageContent">{message}</p>
             <p className="sender">{username}</p>
             <p className="timeStamp">{time}</p>
